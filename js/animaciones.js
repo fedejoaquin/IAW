b5dec2c64fd55c374b5dcf70fefe49e6e3fd266e
Federico_prueba <Federico_prueba@FJ>
2016-02-16 11:55:42 -0300

    Revert "Modificación vista y animaciones de webResto. Agrego imagen facebook."
    This reverts commit 11fe8c082febf81882e034cc0bf8a3fb21d76b3b.

--- js/animaciones.js
+++ js/animaciones.js
@@ -8,22 +8,8 @@ $( document ).ready(function(){
     
     $(".btn-login").show("size");
     
-    $("#btn-login-empleados").click(function() {
-       $("#pnl-opc-login-usuarios").hide();
-       $("#pnl-login-usuarios").hide();
-       $("#pnl-login-empleados").show("size");
+    $(".btn-login").click(function() {
+        $(".panel-login").show("size");
     });
-    
-    $("#btn-login-usuarios").click(function() {
-       $("#pnl-login-empleados").hide();
-       $("#pnl-login-usuarios").hide();
-       $("#pnl-opc-login-usuarios").show("size");
-    });
-    
-    $("#btn-login-usuarios-local").click(function() {
-        $("#pnl-opc-login-usuarios").hide();
-        $("#pnl-login-usuarios").show("size");
-    });
-    
 });
 
