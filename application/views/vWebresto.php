282f0b63fd61f2b76e16e220ff32573f9ac18062
Federico_prueba <Federico_prueba@FJ>
2016-02-16 18:57:08 -0300

    Ruteo solucionado. Saqué el index.php del comienzo. Acomodé los enlaces y rutas en la carga de imágenes y recursos.

--- application/views/vWebresto.php
+++ application/views/vWebresto.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <script src="../js/jquery-2.2.0.min.js" type="text/javascript"></script>
-    <script src="../js/jquery-ui.min.js" type="text/javascript"></script>
-    <script src="../js/materialize.js" type="text/javascript"></script>
-    <script src="../js/animaciones.js" type="text/javascript"></script>
+    <script src="js/jquery-2.2.0.min.js" type="text/javascript"></script>
+    <script src="js/jquery-ui.min.js" type="text/javascript"></script>
+    <script src="js/materialize.js" type="text/javascript"></script>
+    <script src="js/animaciones.js" type="text/javascript"></script>
     
-    <link href="../css/materialize.css" rel="stylesheet" type="text/css"/>  
-    <link href="../css/Estilo.css" rel="stylesheet" type="text/css"/>
-    <link href="../css/jquery-ui.min.css" rel="stylesheet" type="text/css"/>
+    <link href="css/materialize.css" rel="stylesheet" type="text/css"/>  
+    <link href="css/Estilo.css" rel="stylesheet" type="text/css"/>
+    <link href="css/jquery-ui.min.css" rel="stylesheet" type="text/css"/>
     
 </head>
     <body>
