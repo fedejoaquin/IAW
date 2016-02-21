24bc5f29e4505844207ebbc1d94b1c7ffcbb3a76
Federico_prueba <Federico_prueba@FJ>
2016-02-21 18:49:17 -0300

    Merge modelo MEmpleados.

--- application/models/MEmpleado.php
+++ application/models/MEmpleado.php
@@ -1,10 +1,10 @@
 <?php 
 class MEmpleado extends CI_Model {
-
-        public function __construct()
-        {
-                // Call the CI_Model constructor
-                parent::__construct();
+       
+        public function get_empleado_password ($nombre){
+            $query = $this->db->query('SELECT id, password FROM Empleados WHERE nombre="'.$nombre.'"');
+            $resultado = $query->row_array();
+            return $resultado;
         }
 
         public function insertarEmpleado($data){   
