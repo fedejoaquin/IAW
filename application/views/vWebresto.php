e65a67cd8bc4b9a07cd0286e8f37d969e6e94b81
Federico_prueba <Federico_prueba@FJ>
2016-03-13 15:22:29 -0300

    Modificaciones para la visualización de errores en los formularios de Empleados y login de clientes.

--- application/views/vWebresto.php
+++ application/views/vWebresto.php
@@ -4,16 +4,8 @@
     <?php include 'componentes/recursos.phtml'; ?>
 </head>
     <body>
-        <?php include 'componentes/botonera.phtml'; ?> 
-        <?php 
-            $ruta = array(
-                array('Inicio',  site_url()), 
-                array('WebResto 2.0', site_url().'webresto')
-            );
-            include 'componentes/rutaSeguimiento.phtml';
-            include 'componentes/errores.phtml';
-        ?>       
-    <?php include '/webresto/'.$funcion.'.phtml'; ?>           
-    <?php include 'componentes/footer.phtml'; ?>   
+        <?php include 'componentes/botonera.phtml'; ?>      
+        <?php include '/webresto/'.$funcion.'.phtml'; ?>           
+        <?php include 'componentes/footer.phtml'; ?>   
     </body>
 </html>
